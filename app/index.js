810b5d9222256afbab1a7fe2baad464e115dbded
Ermesom <ermesoml.ufg@gmail.com>
2019-03-30 18:40:27 -0300

    Modificação da porta para deploy no heroku

--- app/index.js
+++ app/index.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
-
-// app.use(express.json);
 app.use(require('./routes.js'))
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000')
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
 });
