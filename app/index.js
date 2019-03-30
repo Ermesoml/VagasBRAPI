cd0f6bd8cef4d5424cae740c16f6f1e4d1e3ed72
Ermesom <ermesoml.ufg@gmail.com>
2019-03-30 18:57:44 -0300

    Habilitando cors.

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
+var cors = require('cors')
 
 const app = express();
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
 app.use(require('./routes.js'))
