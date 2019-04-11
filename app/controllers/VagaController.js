9199a239dfae1e21a8287aa4b73548582281e479
Ermesom lourenço <ermesoml.ufg@gmail.com>
2019-04-11 19:15:09 -0300

    Filtering by id

--- app/controllers/VagaController.js
+++ app/controllers/VagaController.js
@@ -3,7 +3,11 @@ const Vaga = require('../models/Vaga');
 module.exports = {
   async index(req, res){
     const vagas = await Vaga.find({}).sort({created_at: -1});
-
     return res.json(vagas);
+  },
+
+  async findById(req, res){
+    const vaga = await Vaga.findOne({_id: req.query.vaga_id});
+    return res.json(vaga);
   },
 }
--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,6 @@ const routes = new express.Router();
 const VagaController = require('./controllers/VagaController.js');
 
 routes.get('/', VagaController.index);
+routes.get('/vaga/:vaga_id', VagaController.findById);
 
 module.exports = routes;
