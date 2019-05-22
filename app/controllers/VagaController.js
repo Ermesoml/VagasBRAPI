8057e76c6ee8523d6f9a5a09cd534cf2103f74eb
Ermesom lourenço <ermesoml.ufg@gmail.com>
2019-05-22 17:38:39 -0300

    Melhorias

--- app/controllers/VagaController.js
+++ app/controllers/VagaController.js
@@ -2,7 +2,13 @@ const Vaga = require('../models/Vaga');
 
 module.exports = {
   async index(req, res){
-    const vagas = await Vaga.find({}).sort({created_at: -1});
+    let pagina = req.query.pagina ? req.query.pagina : 1;
+    let filtroTituloVaga = req.query.filtroTituloVaga ? req.query.filtroTituloVaga : ' ';
+    let quantidade_por_pagina = 12;
+
+    console.log(filtroTituloVaga)
+
+    const vagas = await Vaga.find({title: { $regex: '.*' + filtroTituloVaga + '.*' }}).sort({created_at: -1}).limit(quantidade_por_pagina).skip(quantidade_por_pagina * pagina);
     return res.json(vagas);
   },
 
