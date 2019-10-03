77407d50edea7a5f1fe1c73637e543259442640a
Ermesom <ermesoml.ufg@gmail.com>
2019-10-02 22:42:06 -0300

    Busca Avançada

--- app/controllers/VagaController.js
+++ app/controllers/VagaController.js
@@ -4,34 +4,75 @@ module.exports = {
   async index(req, res){
     let pagina = req.query.pagina ? req.query.pagina : 1;
     let filtroVaga = req.query.filtroTituloVaga ? req.query.filtroTituloVaga : ' ';
+    let tipoFiltro = req.query.tipoFiltroSelecionado ? req.query.tipoFiltroSelecionado : 'S';
     let filtroRepositorio = req.query.filtroRepositorio && req.query.filtroRepositorio != 'Todos' ? req.query.filtroRepositorio : '';
     let quantidade_por_pagina = 12;
 
-    
+    let vagas;
 
-    let filtros = { $or: 
-      [
-        {title: new RegExp(filtroVaga.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&'), 'i')}, 
-        {body: new RegExp (filtroVaga.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&'), 'i')},
-      ],
-    };
-
-    if (filtroRepositorio != ''){
-      filtros.$and = 
-      [
-        {repo_name: new RegExp (filtroRepositorio.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&'), 'i')}
-      ]
-    }
-
-    const vagas = await Vaga.find(
-        filtros, 
+    if (tipoFiltro === 'A'){
+      vagas = await Vaga.aggregate([
         {
-          "created_at" : 1, 
-          "title" : 1,
-          "repo_name" : 1, 
-          "user_avatar_url": 1
+          $match: {
+            $text: {
+              $search: `"\"${new RegExp (filtroVaga.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&'), 'i')}\""`
+            },
+            "repo_name": new RegExp (filtroRepositorio.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&'), 'i')
+          }
+        },
+        
+        {
+          $project: {
+            _id: true,
+            title: true,
+            created_at : 1, 
+            title : 1,
+            repo_name : 1, 
+            user_avatar_url: 1,
+            score: {
+              $meta: 'textScore'
+            }
+          }
+        },
+        {
+          $sort: {
+            score: -1
+          }
+        },
+        {
+          $limit: quantidade_por_pagina * (pagina)
+        },
+        {
+          $skip: quantidade_por_pagina * (pagina - 1)
         }
-    ).sort({created_at: -1}).limit(quantidade_por_pagina).skip(quantidade_por_pagina * (pagina - 1));
+      ]);
+    }
+    else if (tipoFiltro === 'S')
+    {
+      let filtros = { $or: 
+        [
+          {title: new RegExp(filtroVaga.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&'), 'i')}, 
+          {body: new RegExp (filtroVaga.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&'), 'i')},
+        ],
+      };
+  
+      if (filtroRepositorio != ''){
+        filtros.$and = 
+        [
+          {repo_name: new RegExp (filtroRepositorio.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&'), 'i')}
+        ]
+      }
+  
+      vagas = await Vaga.find(
+          filtros, 
+          {
+            "created_at" : 1, 
+            "title" : 1,
+            "repo_name" : 1, 
+            "user_avatar_url": 1
+          }
+      ).sort({created_at: -1}).limit(quantidade_por_pagina).skip(quantidade_por_pagina * (pagina - 1));
+    }
     
     return res.json(vagas);
   },
