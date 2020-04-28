7c6cdf659f6becdad735db6c3caea030fda7b57b
Ermesom Lourenço de Oliveira Barros <ermesoml.ufg@gmail.com>
2020-04-28 15:54:07 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ var cors = require('cors')
 const app = express();
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 app.use(require('./routes.js'))
 
 let port = process.env.PORT || 3000;
